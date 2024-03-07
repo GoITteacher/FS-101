0fd62bf586ed449845b9a8ea337b57106eaa2bca
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-03-07 16:22:45 +0200

    Initialize module-8-1

---
+++ js/03-tags.js
@@ -0,0 +1,4 @@
+let refs = {
+  firstTagsList: document.querySelector('.tags-box[data-group="group-1"]'),
+  secondTagsList: document.querySelector('.tags-box[data-group="group-2"]'),
+};
---
+++ js/04-color-picker.js
@@ -0,0 +1,59 @@
+let colorPalette = [];
+const LENGTH = 5;
+
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < LENGTH; i++) {
+    let hex = getRangomColor();
+    let color = {
+      hex,
+      rgb: hexToRgb(hex),
+    };
+
+    items.push(color);
+  }
+
+  colorPalette = [...items];
+}
+
+function getRangomColor() {
+  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+}
+
+function getRandomHex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, '0');
+}
+
+function hexToRgb(hex) {
+  let red = parseInt(hex.substring(1, 3), 16);
+  let green = parseInt(hex.substring(3, 5), 16);
+  let blue = parseInt(hex.substring(5, 7), 16);
+  return `${red}, ${green}, ${blue}`;
+}
+
+createPaletteItems();
+////////////////////////////////////////////////////////////////////////////
+
+const refs = {
+  itemList: document.querySelector('.js-colors-box'),
+  modalElement: document.querySelector('.modal'),
+  btnReloadColor: document.querySelector('.js-reload-color'),
+  backdropElem: document.querySelector('.js-backdrop'),
+};
+
+////////////////////////////////////////////////////////////////////////////
+
+/* 
+nodeName
+<li class="color-item">
+    <button class="color-body" style="background-color:...;"></button>
+    <div class="color-footer">
+        <div>HEX: ....</div>
+        <div>RGB: ....</div>
+        <div></div>
+    </div>
+</li>
+
+*/
