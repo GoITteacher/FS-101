db1ae30c5d90ef778998e454af8539f23397a7f8
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-03-07 16:22:45 +0200

    Initialize module-7-2

---
+++ js/01-events.js
@@ -0,0 +1,12 @@
+/**
+ * Подія сlick
+ *
+ * - addEventListener
+ * - removeEventListener
+ *
+ * Натискаючи на кнопку "Click me" змусь
+ * червоний квадратик зміщуватись на 50px по діагоналі
+ */
+
+const clickMe = document.querySelector('.js-click');
+const box = document.querySelector('.js-box');
---
+++ js/05-mouse.js
@@ -0,0 +1,7 @@
+/*
+ * Події миші
+ * - mouseenter і mouseleave (це ховер)
+ * - mousemove (chatty event - балакуча подія)
+ */
+
+const boxRef = document.querySelector('.js-box');
