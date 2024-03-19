0858498f79ac75438f665004af7804f4f87999bd
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-03-19 19:27:26 +0200

    update

--- src/js/script.js
+++ src/js/script.js
@@ -7,18 +7,3 @@ import './modules/instagram';
 import './modules/user';
 
 // ====================================
-
-/* function getPostsByUser(userId) {
-  const BASE_URL = 'https://jsonplaceholder.typicode.com';
-  const END_POINT = '/posts';
-  const PARAMS = `?userId=${userId}`;
-  const url = BASE_URL + END_POINT + PARAMS;
-
-  const options = {
-    headers: {
-      test_header: 'Volodka',
-    },
-  };
-
-  return fetch(url, options).then(res => res.json());
-} */
