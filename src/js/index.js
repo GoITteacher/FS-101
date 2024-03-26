a3ce5931c90819ca9e3db6139a5acaa23f56e464
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-03-26 21:50:10 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,74 +1,35 @@
-// async function foo() {
-//   console.log('START');
-
-//   const p1 = await createPromise(4000, 'p1');
-//   console.log(p1);
-
-//   console.log('END');
-// }
-
-// foo();
-
-// =========================
-// async function foo(){}
-
-// const foo = async () => {};
-
-/* const obj = {
-  async showName() {},
-}; */
-
-/* class User {
-  static async showName() {}
-  async showName() {}
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  console.log(n);
+  return n * factorial(n - 1);
 }
-export async function foo() {} */
 
-// [].forEach(async () => {});
+const res = factorial(5);
+console.log(res);
 
-// document.addEventListener('click', async () => {});
+// 120;
+// 5 * 24;
+// 4 * 6;
+// 3 * 2;
+// 2 * 1;
 
-// ==========================
+// =================================
 
-// const p1 = createPromise(100, 'awd');
-// const data = await p1;
-
-// ==========================
-
-// async function foo() {
-//   return 10;
+// function fibba(n) {
+//   if (n <= 1) return 1;
+//   return fibba(n - 1) + fibba(n - 2);
 // }
 
-// const data = foo();
+// 1 1 2 3 5 8 13 21
+// 0 1 2 3 4 5  6
+
+// fibba(0); // 1
+// fibba(1); // 1
+// fibba(2); // 2
+// fibba(3); // 3
+
+// const data = fibba(6);
 
 // console.log(data);
-
-// =========================
-
-/* function getTodos(id) {
-  const url = `https://jsonplaceholder.typicode.com/todos/${id}`;
-  return fetch(url).then(response => response.json());
-}
-
-async function foo() {
-  const promises = [];
-
-  for (let i = 1; i <= 5; i++) {
-    const promise = getTodos(i);
-    promises.push(promise);
-  }
-
-  const result = await Promise.all(promises);
-  console.log(result);
-}
-
-foo();
- */
-
-// ============================
-import axios from 'axios';
-
-function getComments() {
-  const url = 'https://jsonplaceholder.typicode.com/comments';
-  return axios.get(url).then(res => res.data);
-}
