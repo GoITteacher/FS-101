9d3fe65095280b077dc86a04dc4e3fbb5b08dffe
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-03-26 19:12:45 +0200

    update

--- src/js/users.js
+++ src/js/users.js
@@ -1,4 +1,10 @@
-import { UsersAPI } from './modules/usersAPI';
+import {
+  deleteUser,
+  getUsers,
+  resetUser,
+  updateUser,
+} from './modules/usersAPI';
+import { createUser } from './modules/usersAPI';
 
 // ===================================================
 const refs = {
@@ -8,26 +14,101 @@ const refs = {
   resetUserForm: document.querySelector('.js-reset-form'),
   deleteUserForm: document.querySelector('.js-delete-form'),
 };
+
 // ============================================================
 
-const usersAPI = new UsersAPI();
+refs.createUserForm.addEventListener('submit', onCreateUser);
+refs.updateUserForm.addEventListener('submit', onUpdateUser);
+refs.resetUserForm.addEventListener('submit', onResetUser);
+refs.deleteUserForm.addEventListener('submit', onDeleteUser);
 
-usersAPI
-  .getUsers()
-  .then(data => {
-    renderUsers(data.reverse());
-  })
-  .catch(err => {
-    console.log(err);
+function onCreateUser(e) {
+  e.preventDefault();
+
+  const myData = {
+    email: e.target.elements.userEmail.value,
+    name: e.target.elements.userName.value,
+    phone: e.target.elements.userPhone.value,
+    img: `https://source.unsplash.com/720x1280/?random=${Math.random()}&girl,portret,celebrity`,
+  };
+
+  createUser(myData).then(newUser => {
+    const markup = userTemplate(newUser);
+    refs.userListElem.insertAdjacentHTML('afterbegin', markup);
   });
 
-// ==========================================================
+  e.target.reset();
+}
+function onUpdateUser(e) {
+  e.preventDefault();
 
-function templateUser({ id, name, email, phone, img }) {
-  return `
-  <li class="card user-item" data-id="${id}">
+  const myData = {};
+  const formData = new FormData(refs.updateUserForm);
+
+  formData.forEach((value, key) => {
+    if (value) {
+      myData[key] = value;
+    }
+  });
+
+  updateUser(myData).then(updatedUser => {
+    const markup = userTemplate(updatedUser);
+    const oldUser = document.querySelector(`[data-id="${myData.id}"]`);
+    oldUser.insertAdjacentHTML('afterend', markup);
+    oldUser.remove();
+  });
+
+  e.target.reset();
+}
+function onResetUser(e) {
+  e.preventDefault();
+
+  const myData = {};
+  const formData = new FormData(refs.resetUserForm);
+
+  formData.forEach((value, key) => {
+    myData[key] = value;
+  });
+
+  resetUser(myData).then(updatedUser => {
+    const markup = userTemplate(updatedUser);
+    const oldUser = document.querySelector(`[data-id="${myData.id}"]`);
+    oldUser.insertAdjacentHTML('afterend', markup);
+    oldUser.remove();
+  });
+
+  e.target.reset();
+}
+function onDeleteUser(e) {
+  e.preventDefault();
+
+  const id = e.target.elements.userId.value;
+
+  deleteUser(id)
+    .then(() => {
+      const oldUser = document.querySelector(`[data-id="${id}"]`);
+      oldUser.remove();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  e.target.reset();
+}
+
+// ============================================================
+
+getUsers().then(users => {
+  const markup = usersTemplate(users);
+  refs.userListElem.innerHTML = markup;
+});
+
+// ============================================================
+
+function userTemplate({ id, name, img, email, phone }) {
+  return `<li class="card user-item" data-id="${id}">
   <img
-    src="${img}"
+    src="https://source.unsplash.com/720x1280/?random=${id}&girl,portret,celebrity"
     alt="#"
     class="user-avatar"
   />
@@ -38,11 +119,8 @@ function templateUser({ id, name, email, phone, img }) {
 </li>`;
 }
 
-function templateUsers(users) {
-  return users.map(templateUser).join('');
+function usersTemplate(arr) {
+  return arr.map(userTemplate).join('\n\n\n\n');
 }
 
-function renderUsers(users) {
-  const markup = templateUsers(users);
-  refs.userListElem.innerHTML = markup;
-}
+// =======================================
